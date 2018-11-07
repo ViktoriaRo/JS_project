206d6f2be58b6c3af74c0461d5b13ac02c1bab66
victoria <vica.rotaru97@gmail.com>
2018-11-07 11:18:26 +0300

    code refactor

--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -5,20 +5,20 @@ import FeedbackForm from './FeedbackForm';
 class Feedback extends  React.Component {
 	render() {
 		return(
-				<div className="wrapper">
-					<div className="main">
-						<div className="container">
-							<div className="row">
-								<div className="col-5 title-container">
-									<Titles />
-								</div>
-								<div className="col-7 form-container">
-									<FeedbackForm />
-								</div>
+			<div className="wrapper">
+				<div className="main">
+					<div className="container">
+						<div className="row">
+							<div className="col-5 title-container">
+								<Titles />
+							</div>
+							<div className="col-7 form-container">
+								<FeedbackForm />
 							</div>
 						</div>
 					</div>
 				</div>
+			</div>
 		);
 	}
 };
