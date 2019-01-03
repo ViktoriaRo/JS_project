bcf923e3ca833373133a5b9b0a0835400cbc1aea
victoria <vica.rotaru97@gmail.com>
2019-01-03 16:38:27 +0300

    Updates

---
+++ precache-manifest.2149dafd46e0ba0720c677901a527b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64bdb73506c163b64b93",
+    "url": "/JS_project/static/js/runtime~main.64bdb735.js"
+  },
+  {
+    "revision": "aaa7a559d130dc1a8c82",
+    "url": "/JS_project/static/js/main.aaa7a559.chunk.js"
+  },
+  {
+    "revision": "029453094b4f9a6a53eb",
+    "url": "/JS_project/static/js/1.02945309.chunk.js"
+  },
+  {
+    "revision": "aaa7a559d130dc1a8c82",
+    "url": "/JS_project/static/css/main.fe793d94.chunk.css"
+  },
+  {
+    "revision": "029453094b4f9a6a53eb",
+    "url": "/JS_project/static/css/1.41cdd8dc.chunk.css"
+  },
+  {
+    "revision": "3fddbeb24b848a0b0d9688560c02ce7f",
+    "url": "/JS_project/index.html"
+  }
+];
